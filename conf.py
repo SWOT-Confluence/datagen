39bb7f81cb8960e9a7c9e981c35ff8e5c30b8125
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2022-11-11 13:29:29 -0500

    Factor out data needed to write JSON files

---
+++ conf.py
@@ -0,0 +1,11 @@
+conf = {
+    "basin": "basin.json",
+    "reach_id_list": "reach_id.json",
+    "s3_list": "s3_list.json",
+    "s3_cred_endpoints": {
+        'pocloud':'https://archive.podaac.earthdata.nasa.gov/s3credentials',
+        'lpdaac':'https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials',
+        'ornldaac':'https://data.ornldaac.earthdata.nasa.gov/s3credentials',
+        'gesdisc':'https://data.gesdisc.earthdata.nasa.gov/s3credentials'
+    }
+}
