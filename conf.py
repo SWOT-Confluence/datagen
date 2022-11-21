bb56ea30f367c2f2219782d1f2bb6f4ed2e51e61
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2022-11-21 14:03:27 -0500

    Updated for local shapefile list

--- conf.py
+++ conf.py
@@ -6,6 +6,7 @@ conf = {
     "reach": "reaches.json",
     "reach_node": "reach_node.json",
     "s3_list": "s3_list.json",
+    "s3_list_local": "s3_list_local.json",
     "s3_cred_endpoints": {
         'pocloud':'https://archive.podaac.earthdata.nasa.gov/s3credentials',
         'lpdaac':'https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials',
