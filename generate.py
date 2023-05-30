7167f1506e61220b92457988bf05f32323971865
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-05-30 16:08:30 +0000

    Provide documentation on subsetting ops

--- generate.py
+++ generate.py
@@ -16,6 +16,7 @@ Command line arguments:
  -l: indicates local run (optional)
  -j: name of JSON file (optional)
  -f: name of shapefile directory for local runs (optional)
+ -u: Path to JSON file with list of reaches to subset
 
 River Example: python3 generate.py -c river -i 3 -p POCLOUD -s SWOT_SIMULATED_NA_CONTINENT_L2_HR_RIVERSP_V1 -t 2022-08-01T00:00:00Z,2022-08-22T23:59:59Z -d /home/useraccount/json_data
 Lake Example: python3 generate.py -c lake -i 3 -p POCLOUD -s SWOT_SIMULATED_NA_CONTINENT_L2_HR_RIVERSP_V1 -t 2022-08-01T00:00:00Z,2022-08-22T23:59:59Z -d /home/useraccount/json_data
